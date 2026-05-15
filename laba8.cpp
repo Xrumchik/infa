62009b05add25bb9d1e74e64067f94be3ad56b05
Sardelka <110850654+Xrumchik@users.noreply.github.com>
2026-05-15 23:51:18 +0300

    Add files via upload

--- laba8.cpp
+++ laba8.cpp
@@ -7,12 +7,15 @@
 
 class A{
 private:
-    char *title;
     float price;
+protected:
+    char *title;
+    int weight;
     char *department;
     char *country;
-    int weight;
 public:
+    float get_price(){return price;}
+    void set_price(float &p){price=p;}
     void print_el();
     void init_el(FILE *f);
     float obsh_price();
@@ -59,6 +62,47 @@ public:
     }
 };
 
+class Fa : public A{
+private:
+    int cnt_sugar;
+    char *shop;
+protected:
+    int cnt_salt;
+public:
+    int cnt_tasty(){
+        return 10-abs(cnt_salt-cnt_sugar);
+    };
+    Fa(){
+        shop=new char [30];
+    }
+    Fa(char *t, float p, char *dep, char *c, int w, char *s, int sug,int slt):A(t,p,dep,c,w){
+        shop=new char [strlen(s)+1];
+        strcpy(shop,s);
+        cnt_sugar=sug;
+        cnt_salt=slt;
+
+    };
+    Fa(char *t, char *dep, char *c, char *s):A(t,dep,c){
+        shop=new char [strlen(s)+1];
+        strcpy(shop,s);
+        cnt_sugar=5;
+        cnt_salt=4;
+    };
+    Fa(Fa &curr):A(curr.title,curr.get_price(),curr.department,curr.country,curr.weight){
+        shop=new char [strlen(curr.shop)+1];
+        strcpy(shop,curr.shop);
+        cnt_salt=curr.cnt_salt;
+        cnt_sugar=curr.cnt_sugar;
+    }
+    ~Fa(){
+        delete [] shop;
+    }
+    void print_el(){
+        printf("Продукт называет: %s, его стоимость: %.2f, его отдел: %s, его страна производитель: %s, его масса: %d, кол-во соли: %d, кол-во сахара: %d, магазин: %s.\n", title,get_price(),department, country,weight,cnt_salt,cnt_sugar,shop);
+    }
+    void init_el(FILE *f);
+};
+
 
 
 void fa(A arr[10]){
@@ -96,29 +140,41 @@ void A::init_el(FILE *f){
     fscanf(f,"%d",&(weight));
 }
 
+void Fa::init_el(FILE *f){
+    float p;
+    fscanf(f,"%s",title);
+    fscanf(f,"%f",&(p));
+    fscanf(f,"%s",department);
+    fscanf(f,"%s",country);
+    fscanf(f,"%d",&(weight));
+    fscanf(f,"%d",&(cnt_sugar));
+    fscanf(f,"%d",&(cnt_salt));
+    fscanf(f,"%s",shop);
+    set_price(p);
+}
+
 
 int main(){
     SetConsoleOutputCP(65001);
-    A arr[10];
-    FILE *ishod=fopen("laba1.txt","r");
+    Fa arr[10];
+    FILE *ishod=fopen("laba8.txt","r");
     for(int i=0;i<10;i++){
         arr[i].init_el(ishod);
         arr[i].print_el();
-        printf("Общая цена продукта №%d: %.2f\n", i,arr[i].obsh_price());
+        printf("Уровень вкуса продукта №%d: %d\n", i,arr[i].cnt_tasty());
     }
     puts("Конструктор по умолчанию");
-    A s1;
+    Fa s1;
     s1.print_el();
     puts("Конструктор копирования");
-    A s2=arr[9];
+    Fa s2=arr[9];
     s2.print_el();
     puts("Конструктор полностью");
-    A s3("Apple",123,"Fruit","Somali",321);
+    Fa s3("Yabkoko",123,"Frukti","Somali",321,"KupiShop",7,3);
     s3.print_el();
     puts("Конструктор только с символами");
-    A s4("Apple","Fruit","Somali");
+    Fa s4("Yabkoko","Frukti","Somali", "U_Doma");
     s4.print_el();
-    // fa(arr);
     fclose(ishod);
     return 0;
 }
